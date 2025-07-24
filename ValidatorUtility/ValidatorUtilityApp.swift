906220f0a7494a2f1a876a05d9d6cb55ae64c059
Tahir Anil Oghan <t.anil.oghan@gmail.com>
2025-07-25 01:24:16 +0300

    Initial Commit

---
+++ ValidatorUtility/ValidatorUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidatorUtilityApp.swift
+//  ValidatorUtility
+//
+//  Created by Tahir Anil Oghan on 25.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ValidatorUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
